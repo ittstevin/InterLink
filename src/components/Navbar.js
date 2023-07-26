c0438829058d91e0f4da3c46d1f83b69b3216ca2
ittstevin <tevingichoya@gmail.com>
2023-07-26 12:12:01 +0300

    updated css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,25 @@
-import React, { useContext } from 'react'
-import {signOut} from "firebase/auth"
-import { auth } from '../firebase'
-import { AuthContext } from '../context/AuthContext'
+import React, { useContext } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
-  const {currentUser} = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
 
   return (
-    <div className='navbar'>
-      <span className="logo">InterL!nk</span>
+    <>
+    <div>
+    <span className="logo">InterL!nk</span>
+    </div>
+    <div className="navbar">
       <div className="user">
-        <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
-        <button onClick={()=>signOut(auth)}>logout</button>
+        <img src={currentUser.photoURL} alt="" />
+        <button onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
-  )
-}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Search from "./Search";
 import Chats from "./Chats";
 
 const Sidebar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen((prevState) => !prevState);
-  };
 
   return (
-    <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+    <div className="sidebar">
       <Navbar />
       <Search />
       <Chats />
-      <button className="toggleSidebarBtn" onClick={toggleSidebar}>
-        Toggle Sidebar
-      </button>
     </div>
   );
 };
